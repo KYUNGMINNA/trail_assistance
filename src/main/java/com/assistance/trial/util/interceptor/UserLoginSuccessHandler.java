5d16e3bf73c592a005b62c670a4c2e27881fb163
KYUNGMINNA <nkm4095@naver.com>
2022-10-21 13:43:44 +0900

    account login interceptor add

--- src/main/java/com/assistance/trial/util/interceptor/UserLoginSuccessHandler.java
+++ src/main/java/com/assistance/trial/util/interceptor/UserLoginSuccessHandler.java
@@ -9,6 +9,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.assistance.trial.command.AccountVO;
+
 
 public class UserLoginSuccessHandler implements HandlerInterceptor{
  
@@ -20,7 +22,30 @@ public class UserLoginSuccessHandler implements HandlerInterceptor{
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
+		System.out.println("로그인 인터셉터 동작!");
 		
+		
+		 ModelMap mv=modelAndView.getModelMap();
+		 AccountVO vo=(AccountVO)mv.get("user");
+		 //위 아래 같은 코드 같은 기능 
+		 //UserVO vo=(UserVO)modelAndView.getModel().get("user");
+		
+		 System.out.println("interceptor vo:"+vo);
+		
+		
+		if(vo ==null) {// 로그인 실패.
+			modelAndView.addObject("msg", "loginFail");
+			modelAndView.setViewName("/user/login");
+		}else {
+			//컨트롤러에서 로그인을 성공했던 사람 
+			System.out.println("로그인 성공 로직 동작!");
+			
+			//로그인 성공한 회원에게 세션 데이터를 생성해서 로그인 유지를 하게 해 줌
+			HttpSession session=request.getSession();
+			session.setAttribute("login", vo);			
+			response.sendRedirect(request.getContextPath()); //rootcontext  request.getContextPath바뀔 가능성 때문에 ,이렇게씀
+		
+		}
 		
 		
 	}
